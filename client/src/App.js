2c40f590c6b02e29938b456b62a614c132051f92
UROS <nonamen212@gmail.com>
2021-01-05 23:59:10 +0100

    Init commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Provider} from 'react-redux'
+import store from './store'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={App}/>
+            <Route path="/pretraga" exact component={App}/>
+            <Route path="/:id" exact component={App}/>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
